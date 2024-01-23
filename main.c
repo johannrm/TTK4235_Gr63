3dbad7f90469907d600dded41962748c8af4ca11
Johannes Munkvold <johannrm@stud.ntnu.no>
2024-01-23 19:35:12 +0100

    main-fil fra Lab 2

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+int main() {
+	printf("Hei fra Gruppe 63!\n");
+	printf("HALLO TILBAKE GR63!\n");
+
+	printf("..og andre grupper :/\n");
+
+       if(1 > 0) {
+		return 1;
+	}
+
+
+	return 0;
+}
